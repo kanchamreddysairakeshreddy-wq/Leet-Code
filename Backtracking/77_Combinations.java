45e7d97132e824d83aab0cc8cbdfa0d4a9363076
K Sai Rakesh Reddy <kanchamreddysairakeshreddy@gmail.com>
2026-06-02 17:00:01 +0530

    77.Combinations

---
+++ Backtracking/77_Combinations.java
@@ -0,0 +1,34 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+
+    List<List<Integer>> result = new ArrayList<>();
+
+    public List<List<Integer>> combine(int n, int k) {
+
+        backtrack(1, n, k, new ArrayList<>());
+
+        return result;
+    }
+
+    private void backtrack(int start,
+                           int n,
+                           int k,
+                           List<Integer> current) {
+
+        if (current.size() == k) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+
+            current.add(i);
+
+            backtrack(i + 1, n, k, current);
+
+            current.remove(current.size() - 1);
+        }
+    }
+}
